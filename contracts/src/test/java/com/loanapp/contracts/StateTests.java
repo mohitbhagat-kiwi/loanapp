2dfd3d4cd47f5072a5818f69d525c0f5d5478bf4
mohitbhagat-kiwi <mohit.bhagat@kiwitech.com>
2022-05-23 17:08:05 +0530

    Initial-push

---
+++ contracts/src/test/java/com/loanapp/contracts/StateTests.java
@@ -0,0 +1,14 @@
+package com.loanapp.contracts;
+
+import com.loanapp.states.TemplateState;
+import org.junit.Test;
+
+public class StateTests {
+
+    //Mock State test check for if the state has correct parameters type
+    @Test
+    public void hasFieldOfCorrectType() throws NoSuchFieldException {
+        TemplateState.class.getDeclaredField("msg");
+        assert (TemplateState.class.getDeclaredField("msg").getType().equals(String.class));
+    }
+}
